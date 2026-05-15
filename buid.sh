fba552507484e7e89c512a94ff3706907e5a35cb
Paoo <thienhanaylai@gmail.com>
2026-05-16 00:10:15 +0700

    add: file build .sh

---
+++ buid.sh
@@ -0,0 +1,16 @@
+
+set -e
+
+echo "--- Bắt đầu quá trình cập nhật và triển khai ---"
+
+echo "[1/3] Đang thực hiện git pull..."
+git pull
+
+echo "[2/3] Đang thực hiện npm run build -f..."
+npm run build -f
+
+echo "[3/3] Đang thực hiện docker compose up -d --build..."
+docker compose up -d --build
+
+echo "--- Quá trình hoàn tất thành công! ---"
+"""
