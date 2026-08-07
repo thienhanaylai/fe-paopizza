e9c56147456205c8e4ce601aa6b25cefd915a124
Paoo <thienhanaylai@gmail.com>
2026-08-07 17:45:14 +0700

    update build scripts

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ echo "--- Bắt đầu quá trình cập nhật và triển khai ---"
 echo "[1/3] Đang thực hiện git pull..."
 git pull
 
-echo "[2/3] Đang thực hiện npm run build -f..."
+echo "[2/3] Đang thực hiện update và npm run build -f..."
+npm i -f
 npm run build -f
 
 echo "[3/3] Đang thực hiện docker compose up -d --build..."
